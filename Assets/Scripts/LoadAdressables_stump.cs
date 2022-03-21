6959dcfdb08edcf373ede6aed3e8ed8b62834ea4
rihoishiyama <riho.1112.mj@gmail.com>
2022-03-21 16:56:35 +0900

    temp create stump stage

---
+++ Assets/Scripts/LoadAdressables_stump.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class LoadAdressables_stump : MonoBehaviour
+{
+    private AsyncOperationHandle<GameObject> _handle;
+
+    private async void Start()
+    {
+        // ロード & インスタンス化
+        _handle = Addressables.InstantiateAsync("MainGame_stump", Vector3.zero, Quaternion.identity);
+        await _handle.Task;
+    }
+
+    private void OnDestroy()
+    {
+        // 解放
+        Addressables.ReleaseInstance(_handle);
+    }
+}
